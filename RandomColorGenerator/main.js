68498be15b22c6965569c9224b48a851634ea68a
algorithm <odjaidri@gmail.com>
2019-01-21 21:03:54 +0100

    video#01- Random Color Generator update main.js file

--- RandomColorGenerator/main.js
+++ RandomColorGenerator/main.js
@@ -1,11 +1,3 @@
-/*
-
-- color syntax => #FFFFFF;
-- Math.floor()
-- Math.random()
-
-*/
-
 function getColor() {
     var color = "#";
     var symbols = "0123456789abcdef";
@@ -13,7 +5,6 @@ function getColor() {
     for (var i = 0; i < 6; i++) {
         color = color + symbols[Math.floor(Math.random() * 16)];
     }
-    console.log(color);
     document.body.style.background = color;
     document.querySelector("#color-generated").textContent = color;
 }
